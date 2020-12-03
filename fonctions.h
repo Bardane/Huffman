83e0d5d219f6156fdc5d1504c7438d5560ca4527
JouanelR <73125997+JouanelR@users.noreply.github.com>
2020-12-03 17:58:36 +0100

    Update fonctions.h
    ajout decodage fonction

--- fonctions.h
+++ fonctions.h
@@ -4,5 +4,7 @@
 void Conv_txt_to_bin(char* nom_txt); // Affichage
 unsigned int* Conv_char_to_bin(char caractere); 
 int Nb_char_txt(char* nom_txt); 
+void Decodage_txt(char* nomdico,char* nomaconv);
+char bintochar(char* encour,FILE* dico_txt);
 
 #endif
