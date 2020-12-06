26babc1b0e9b579ca90223c2adef617c411cb143
JouanelR <73125997+JouanelR@users.noreply.github.com>
2020-12-06 18:52:27 +0100

    Update dico.h
    ajout doxigen

--- dico.h
+++ dico.h
@@ -9,10 +9,136 @@ typedef struct Noeud_Dico{
     struct Noeud_Dico* Sad;
 }Noeud_Dico;
 
-void Creation_Dico(Noeud2* Ab_Huffman, Noeud_Dico** dico);
-void print_tree_dico(Noeud_Dico* tree);
-void Codage_Texte(Noeud_Dico* dico, char* nom_txt);
+void reset(char* chaine, int taille);
+/**
+ * \fn reset(char* chaine, int taille)
+ * \brief reset une chaine de char
+ * 
+ * \param la chaine a reset et sa taille
+ */
 
-void free_tree_dico(Noeud_Dico* dico);
+Noeud_Dico* create_node_dico(char caractere, char* code);
+/**
+ * \fn create_node_dico(char caractere, char* code)
+ * \brief ?
+ * 
+ * \param ?
+ * \return ?
+ */
+
+void print_tree_dico(Noeud_Dico* tree);
+/**
+ * \fn print_tree_dico(Noeud_Dico* tree)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+void free_tree_dico(Noeud_Dico* tree);
+/**
+ * \fn free_tree_dico(Noeud_Dico* tree)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+int depth_dico(Noeud_Dico* tree);
+/**
+ * \fn depth_dico(Noeud_Dico* tree)
+ * \brief ?
+ * 
+ * \param ?
+ * \return ?
+ */
+
+void add_node_BST_dico(Noeud_Dico** tree, char caractere, char* code);
+/**
+ * \fn add_node_BST_dico(Noeud_Dico** tree, char caractere, char* code)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+int get_min_BST_perso_dico(Noeud_Dico* tree);
+/**
+ * \fn get_min_BST_perso_dico(Noeud_Dico* tree)
+ * \brief ?
+ * 
+ * \param ?
+ * \return ?
+ */
+
+int bf_dico(Noeud_Dico* tree);
+/**
+ * \fn bf_dico(Noeud_Dico* tree)
+ * \brief ?
+ * 
+ * \param ?
+ * \return ?
+ */
+
+void right_rotation_dico(Noeud_Dico** tree);
+/**
+ * \fn right_rotation_dico(Noeud_Dico** tree)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+void left_rotation_dico(Noeud_Dico** tree);
+/**
+ * \fn left_rotation_dico(Noeud_Dico** tree)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+void balance_dico(Noeud_Dico** tree);
+/**
+ * \fn balance_dico(Noeud_Dico** tree)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+void add_node_AVL_dico(Noeud_Dico** tree, char caractere, char* code);
+/**
+ * \fn add_node_AVL_dico(Noeud_Dico** tree, char caractere, char* code)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+void creer_dico(Noeud2* Ab, Noeud_Dico** dico, char* code, int i);
+/**
+ * \fn creer_dico(Noeud2* Ab, Noeud_Dico** dico, char* code, int i)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+void ecriture_dico(Noeud_Dico* dico, FILE* Dico);
+/**
+ * \fn ecriture_dico(Noeud_Dico* dico, FILE* Dico)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+void Creation_Dico(Noeud2* Ab_Huffman, Noeud_Dico** dico);
+/**
+ * \fn Creation_Dico(Noeud2* Ab_Huffman, Noeud_Dico** dico)
+ * \brief ?
+ * 
+ * \param ?
+ */
+
+void Codage_Texte(Noeud_Dico* dico, char* nom_txt);
+/**
+ * \fn Codage_Texte(Noeud_Dico* dico, char* nom_txt)
+ * \brief ?
+ * 
+ * \param ?
+ */
 
 #endif
